fc70d8a194667a75b092d6a28966264ac658d0ab
Gustavz7 <gustavo.millanao2020@twk.cl>
2020-06-07 00:56:41 +0200

    modifico pruebas xd, solo probando hol,asdaslñdfuwpfmsdjfgsmv,jñkoigvj,o (lul)

--- venv/tabla_multiplicar.py
+++ venv/tabla_multiplicar.py
@@ -1,2 +1,18 @@
 #holaholaholaholaholaholaholaholaholaaa
-print("hola esto es un mensaje de cualquier cosa")
+print("comandos git")
+print("git add.")#agregar o versionar archivos
+print("git commit -m")#subir las fuentes con un mensaje
+print("git push -u origin master")#subir archivo a la weab
+
+#tabla de multiplicar
+numero=int(input("ingrese un numero para saber su tabla de multiplicar"))
+print(numero,"x 1","=",1*numero)
+print(numero,"x 2","=",2*numero)
+print(numero,"x 3","=",3*numero)
+print(numero,"x 4","=",4*numero)
+print(numero,"x 5","=",5*numero)
+print(numero,"x 6","=",6*numero)
+print(numero,"x 7","=",7*numero)
+print(numero,"x 8","=",8*numero)
+print(numero,"x 9","=",9*numero)
+print(numero,"x 10","=",10*numero)
