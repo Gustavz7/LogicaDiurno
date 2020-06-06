95cb736b351cb7b6f8e84033bb289c9d5fc6d966
Gustavz7 <gustavo.millanao2020@twk.cl>
2020-06-06 23:47:12 +0200

    first commit

---
+++ venv/Operadores.py
@@ -0,0 +1,20 @@
+print("operadores aritmeticos")
+suma=2+3
+resta=6-3
+multiplicacion=2*5
+division=7/3
+resto=7%3
+parteEntera=7//3
+elevado=3**4
+
+#print
+print("la suma es:", suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(resto)
+print(parteEntera)
+print(elevado)
+
+git config --global user.name"Gustavz7"
+git config --global user.email"gustavo.millanao2020@twk.cl"
---
+++ venv/clase04jul.py
@@ -0,0 +1,5 @@
+nombre=input("ingresa tu nombre man")
+print(nombre.upper())#pasa todo a mayuscula
+print(nombre.lower())#pasa todo a minuscula
+opcion=input("tienes covid19? {Y/N}")
+print(opcion)
